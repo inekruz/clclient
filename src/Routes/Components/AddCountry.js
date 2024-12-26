4b05e57f8904fb75027b7da067b3b91b3b49fc19
ArtemSilin1 <glitchplague6qq@gmail.com>
2024-12-26 19:05:35 +0500

    countries panel added

--- src/Routes/Components/AddCountry.js
+++ src/Routes/Components/AddCountry.js
@@ -1,43 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Routes.css';
 
+import Cross from '../RoutesIcons/white_cross.svg';
+
 function AddCountry() {
+
+  const [countryName, setCountryName] = useState('');
+  const [countryCode, setCountryCode] = useState('');
+  const [countryDesc, setCountryDesc] = useState('');
+  const [goodMessage, setGoodMessage] = useState(false);
+
+  const handleCountryAdd = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://api.glimshop.ru/addcountry', {
+        method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+               country_name: countryName,
+               country_code: countryCode,
+               description: countryDesc,
+            }),
+      });
+
+      if(!response.ok) {
+        alert('Ошибка при отправке запроса');
+      } else {
+        setGoodMessage(true);
+        return;
+      }
+    }
+    catch(err) {
+      alert('Ошибка', err)
+    }
+  }
+
   return (
     <div className='control_panel'>
       <h4>Добавить страну</h4>
       
-      <form>
+      <form onSubmit={handleCountryAdd}>
         
         <div>
           <label>Название страны</label>
           <input
-            placeholder='Имя'
+            placeholder='Название страны'
             type='text'
             className='modal_input'
+            onChange={(e) => setCountryName(e.target.value)}
           />
         </div>
         
         <div>
           <label>Код страны</label>
           <input
-            placeholder='Фамилия'
+            placeholder='Код страны'
             type='text'
             className='modal_input'
+            onChange={(e) => setCountryCode(e.target.value)}
           />
         </div>
 
         <div>
           <label>Описание</label>
           <input
-            placeholder='Почта'
-            type='email'
+            placeholder='Описание'
+            type='text'
             className='modal_input desc_input'
+            onChange={(e) => setCountryDesc(e.target.value)}
           />
         </div>
 
         <button className='list_button'>Добавить</button>
 
       </form>
+
+      <div className={`good_message ${goodMessage ? 'active' : ''}`}>
+        <div className='good_message_header'>
+          <img src={Cross} alt='Крестик' className='white_cross' onClick={() => setGoodMessage(false)} />
+        </div>
+        <div className='good_message_content'>
+          <p>Успешно</p>
+        </div>
+      </div>
     </div>
   );
 }
